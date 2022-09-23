e0b53a860386080a95622f0a1caab1c8cd3636fd
PhilipGutberlet <92990487+PhilipGutberlet@users.noreply.github.com>
2022-09-23 12:15:23 +0200

    sicheristsicher

--- ProgrammierprojektBonnWasGeht/userdataformular.js
+++ ProgrammierprojektBonnWasGeht/userdataformular.js
@@ -1,5 +1,21 @@
-function saveUserData(){
-    var age=document.getElementById(`alter`);
-    var weekday=document.getElementById(`weekday`);
-    sessionStorage.setItem(`age`, `weekday`);
+let button = document.getElementById("losghets");
+button.addEventListener("click", machWas());
+
+function machWas(event){
+        event.preventDefault();
+        let weekday=document.getElementById("weekday");
+        console.log(weekday.value);
 }
+
+/*function saveUserData(){
+    event.preventDefault();
+    let weekday = document.getElementById('weekday').ariaValueMax;
+    console.log(weekday)
+    
+}*/
+
+function getWeekday(){
+    return sessionStorage.getItem('weekday');
+}
+
+
